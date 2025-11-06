4acb6263c7c34406a363e263fdf0d901540d867e
Arjesh <arjeshvadadorioya+github@gmail.com>
2025-11-06 22:04:44 +0530

    fix: wrong insta link

--- app/src/main/java/com/arjesh/boxboxassignment/utils/Contants.kt
+++ app/src/main/java/com/arjesh/boxboxassignment/utils/Contants.kt
@@ -5,10 +5,9 @@ const val API_VERSION = "v1"
 
 //links
 const val MEDIUM_PAGE_LINK = "https://blog.boxbox.club/tagged/beginners-guide"
-const val INSTA_LINK = "https://blog.boxbox.club/tagged/beginners-guide"
+const val INSTA_LINK = "https://www.instagram.com/boxbox_club/"
 
 //date & time
 const val UPCOMING_SESSION_DISPLAY_DATE = "dd EEEE"
 const val UPCOMING_SESSION_DISPLAY_TIME = "hh:mm"
-const val UPCOMING_START_END_DISPLAY_TIME = "dd - dd "
 const val TIME_MERIDIEM = "a"
